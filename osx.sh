bbcd15095ddedcdc7a155b25ed6e55d5535cada8
Will Ho <ikaruswill.ho@gmail.com>
2017-04-15 16:22:16 +0800

    Add OS X script

---
+++ osx.sh
@@ -0,0 +1,9 @@
+# Update OS X
+sudo softwareupdate -iva
+
+# Install Xcode command line tools
+xcode-select --install
+
+brew install dark-mode # Activate OS X dark mode via cli
+dark-mode on
+brew remove dark-mode
