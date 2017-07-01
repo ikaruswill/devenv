3783bd2e462210e18403fc2b7f92342bdfdad9e3
Will Ho <ikaruswill.ho@gmail.com>
2017-07-02 03:39:08 +0800

    Add inconsolata font download and install

--- osx.sh
+++ osx.sh
@@ -7,3 +7,12 @@ xcode-select --install
 brew install dark-mode # Activate OS X dark mode via cli
 dark-mode on
 brew remove dark-mode
+
+# Install inconsolata font family
+mkdir inconsolata
+pushd inconsolata
+wget https://github.com/google/fonts/blob/master/ofl/inconsolata/Inconsolata-Regular.ttf
+wget https://github.com/google/fonts/blob/master/ofl/inconsolata/Inconsolata-Bold.ttf
+cp * ~/Library/Fonts/
+popd
+rm -r inconsolata
