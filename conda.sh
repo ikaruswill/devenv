3281eb2ef2d777c319014f14817906cd62c6f338
Will Ho <ikaruswill.ho@gmail.com>
2018-05-14 22:48:08 +0800

    Remove step that creates a general purpose python3 conda environment

--- conda.sh
+++ conda.sh
@@ -13,9 +13,3 @@ source ~/.bash_profile
 
 # Add conda forge repository
 conda config --add channels conda-forge
-
-# Create default python3 general purpose environment
-yes | conda create --name conda python=3
-source activate conda
-yes | conda install scikit-learn pandas nltk spacy flask awscli
-source deactivate
