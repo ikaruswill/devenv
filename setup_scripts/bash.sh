8d351fc09b127878a4ec1158fa6a0c4301304646
Will Ho <ikaruswill.ho@gmail.com>
2018-10-03 00:54:19 +0800

    Deprecate old setup method with shell scripts for ansible method

--- setup_scripts/bash.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-cp configs/bash_profile ~/.bash_profile
-source ~/.bash_profile
--- setup_scripts/git.sh
+++
@@ -1,2 +0,0 @@
-cp configs/gitignore_global ~/.gitignore_global
-git config --global core.excludesfile ~/.gitignore_global
